20f400069c85cd219445b97b6ae6d19e8449fefd
RexvenID <raihan.smp3tp@gmail.com>
2025-04-04 23:56:25 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Contoh script untuk interaktivitas (misalnya animasi atau pop-up)
+document.querySelectorAll('.buy-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Produk berhasil ditambahkan ke keranjang!');
+    });
+});
