93f648c3edb780a6670208639627c3b6271f9364
RexvenID <raihan.smp3tp@gmail.com>
2025-04-04 23:54:34 +0700

    Delete Market directory

--- Market/jss/script.js
+++
@@ -1,6 +0,0 @@
-// Contoh script untuk interaktivitas (misalnya animasi atau pop-up)
-document.querySelectorAll('.buy-btn').forEach(button => {
-    button.addEventListener('click', () => {
-        alert('Produk berhasil ditambahkan ke keranjang!');
-    });
-});
